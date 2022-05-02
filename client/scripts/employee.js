830be21891b952ffcb83997ac896dcbacc3b68f9
Nathan <najohnston4@crimson.ua.edu>
2022-05-01 23:21:56 -0500

    stuff

--- client/scripts/employee.js
+++ client/scripts/employee.js
@@ -1,56 +1,6 @@
 const baseUrl2 = "https://localhost:6969/api/timesheet";
 // $('#main').toggleClass(localStorage.toggled);
 
-// function darkLight() {
-//   /*DARK CLASS*/
-//   if (localStorage.toggled != 'dark') {
-//     $('#main, p').toggleClass('dark', true);
-//     localStorage.toggled = "dark";
-
-//   } else {
-//     $('#main, p').toggleClass('dark', false);
-//     localStorage.toggled = "light";
-//   }
-// }
-
-// /*Add 'checked' property to input if background == dark*/
-// if ($('main').hasClass('dark')) {
-//    $( '#checkBox' ).prop( "checked", true )
-// } else {
-//   $( '#checkBox' ).prop( "checked", false )
-// }
-
-// $("#main").toggleClass(localStorage.toggled);
-
-// function darkLight() {
-//   /*DARK CLASS*/
-//   if (localStorage.toggled != "dark") {
-//     $("#main, p").toggleClass("dark", true);
-//     localStorage.toggled = "dark";
-//   } else {
-//     $("#main, p").toggleClass("dark", false);
-//     localStorage.toggled = "";
-//   }
-// }
-
-// function getUser() {
-
-//   const allUsersApiUrl = baseUrl;
-//   fetch(allUsersApiUrl).then(function(response) {
-//       return response.json();
-//   }).then(function(json) {
-//       userList = json;
-//       json.forEach((user) => {
-//           //console.log(user);
-//           //console.log(user);
-//           if(user.username == searchID && user.password == searchPass) {
-//           }
-//       });
-
-//   }).catch(function(error) {
-//       console.log(error);
-//   });
-// }
 
 function submitLog() {
   const postTimeSheetApiUrl = baseUrl2;
@@ -112,27 +62,7 @@ function ClockInOut(element) {
   child.innerHTML = blabel;
 }
 
-// // Dark Mode LOCAL Storage
-// $('#main').toggleClass(localStorage.toggled);
 
-// function darkLight() {
-//   /*DARK CLASS*/
-//   if (localStorage.toggled != 'dark') {
-//     $('#main, p').toggleClass('dark', true);
-//     localStorage.toggled = "dark";
-
-//   } else {
-//     $('#main, p').toggleClass('dark', false);
-//     localStorage.toggled = "";
-//   }
-// }
-
-// /*Add 'checked' property to input if background == dark*/
-// if ($('main').hasClass('dark')) {
-//    $( '#checkBox' ).prop( "checked", true )
-// } else {
-//   $( '#checkBox' ).prop( "checked", false )
-// }
 
 // Dark Mode SESSION Storage
 $("#body").toggleClass(sessionStorage.toggled);
